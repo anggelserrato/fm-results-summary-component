b6087aeadfd39e0574907219fa316f0ad234b913
Angel Serrato <serrato.anggel@gmail.com>
2026-05-11 20:44:43 -0500

    feat: initialize project with react, vite, and tailwindcss

---
+++ src/components/ResultsSummary.jsx
@@ -0,0 +1,9 @@
+function ResultsSummary() {
+  return (
+    <div>
+      <h1 className="underline text-7xl font-extrabold">ResultsSummary</h1>
+    </div>
+  );
+}
+
+export default ResultsSummary;
