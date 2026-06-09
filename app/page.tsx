bc50e094ba80fa71113f465c296ff2463d3a78c4
Angel Serrato <serrato.anggel@gmail.com>
2026-06-09 16:22:43 -0500

    feat: add responsive tablet layout for results summary component

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,26 @@ import dataJson from './data/data.json';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <section className="flex w-full max-w-md flex-col gap-300">
-        <div className="-end flex w-full flex-col items-center gap-300 rounded-b-4xl bg-linear-to-b from-gradient-2-start to-gradient-2-end px-14.25 py-400 shadow-card">
-          {/* from-gradient-1-start to-gradient-1-end */}
+    <main className="flex min-h-screen flex-col items-center md:bg-blue-50">
+      <section className="flex w-full max-w-md flex-col gap-300 md:mt-[284px] md:grid md:max-w-[686px] md:grid-cols-2 md:gap-0 md:rounded-4xl md:bg-white">
+        <div className="flex w-full flex-col items-center gap-300 rounded-b-4xl bg-linear-to-b from-gradient-2-start to-gradient-2-end px-14.25 py-400 shadow-card md:gap-400 md:rounded-4xl md:px-[69px] md:py-[45px]">
           <p className="text-preset-5-bold text-navy-200">Your Result</p>
-          <div className="flex h-35 w-35 flex-col items-center justify-center rounded-full bg-linear-to-b from-gradient-1-start to-gradient-1-end/1">
-            {/* from-gradient-2-start to-gradient-2-end */}
+          <div className="flex h-35 w-35 flex-col items-center justify-center rounded-full bg-linear-to-b from-gradient-1-start to-gradient-1-end/1 md:h-50 md:w-50">
             <p className="text-preset-2 text-white">76</p>
-            <p className="text-preset-6-bold text-navy-200">of 100</p>
+            <p className="text-preset-6-bold text-navy-200 md:text-preset-5-bold">
+              of 100
+            </p>
           </div>
-          <div className="flex flex-col items-center gap-100 text-center">
-            <p className="text-preset-4 text-white">Great</p>
-            <p className="text-preset-6-medium text-navy-200">
+          <div className="flex flex-col items-center gap-100 text-center md:gap-200">
+            <p className="text-preset-4 text-white md:text-preset-3">Great</p>
+            <p className="text-preset-6-medium text-navy-200 md:text-preset-5-medium">
               You scored higher than 65% of the people who have taken these
               tests.
             </p>
           </div>
         </div>
 
-        <div className="flex flex-col gap-300 px-400">
+        <div className="flex flex-col gap-300 px-400 md:gap-400 md:px-[40px] md:py-[45px]">
           <p className="text-preset-5-bold text-navy-950">Summary</p>
           <div className="flex flex-col">
             <div className="flex flex-col gap-200">
@@ -51,7 +51,7 @@ export default function Home() {
               ))}
             </div>
           </div>
-          <button className="w-full rounded-full bg-navy-950 from-gradient-2-start to-gradient-2-end py-4 text-preset-5-bold text-white hover:bg-linear-to-b">
+          <button className="w-full rounded-full bg-navy-950 from-gradient-2-start to-gradient-2-end py-4 text-preset-5-bold text-white hover:bg-linear-to-b md:text-[1.125rem]">
             Continue
           </button>
         </div>
